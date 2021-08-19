595ea4337b4197465194192e287bee3e7eb926ec
konjacdesert5289 <56007373+konjacdesert5289@users.noreply.github.com>
2021-08-19 12:44:27 +0900

    ポーズ対応

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 window.addEventListener("load", () => {
+  let buttonState: "start" | "pause" = "start";
+  let animID: number;
+  let startTime = 0;
+  let pauseTime = 0;
   const button = document.querySelector("button");
-  button?.addEventListener("click", () => {
-    button.disabled = true;
-    const hw = document.querySelector("input")?.value ?? 0;
-    const startTime = performance.now();
+  const input = document.querySelector("input");
+  const startFunc = () => {
+    const hw = input?.value ?? 0;
+    startTime = startTime + (performance.now() - pauseTime);
     const big = document.querySelector("#big");
     const small = document.querySelector("#small");
     if (!big || !small) return;
     const func = () => {
-      requestAnimationFrame(func);
+      animID = requestAnimationFrame(func);
       const now = performance.now();
       const wage = (+hw * (now - startTime)) / (1000 * 60 * 60);
       const nowString = wage.toFixed(2);
@@ -18,5 +22,23 @@ window.addEventListener("load", () => {
         small.innerHTML = `.${nowString.split(".")[1]}`;
     };
     func();
+  };
+  const pauseFunc = () => {
+    cancelAnimationFrame(animID);
+    pauseTime = performance.now();
+  };
+  button?.addEventListener("click", () => {
+    switch (buttonState) {
+      case "start":
+        startFunc();
+        buttonState = "pause";
+        break;
+      case "pause":
+        pauseFunc();
+        buttonState = "start";
+        break;
+    }
+    button.innerText = buttonState;
+    if (input) input.disabled = buttonState == "pause";
   });
 });
